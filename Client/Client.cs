d9477c9a921f85fef088416da16ebd2c6c22db32
David Wolf <d.wolf@live.at>
2014-03-13 17:47:44 +0100

    Erster Code.

--- Client/Client.cs
+++ Client/Client.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +13,32 @@ namespace Client
 {
 	class Client
 	{
+		private Client() 
+		{
+			mClient = new TcpClient(mHostname, mPort);
+			mStream = mClient.GetStream();
+			mSsl = new SslStream(mStream);
+		}
+
+		public static Client Instance
+		{
+			get 
+			{
+				return sClient;
+			}
+		}
+
+		public Stream GetStream() 
+		{
+			return mStream;
+		}
+
+		private TcpClient mClient = null;
+		private string mHostname = "";
+		private int mPort = 12345;
+		private NetworkStream mStream = null;
+		private SslStream mSsl = null;
+		private X509Certificate2 _cert = new X509Certificate2("Cert/selfSigned.pfx", "password");
+		private static readonly Client sClient = new Client();
 	}
 }
