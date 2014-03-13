e19b0abc8a2448937b6d47d71c845aefda0b4135
Leonhardt Schwarz <if12b076@technikum-wien.at>
2014-03-13 16:09:55 +0100

    blub init

---
+++ Server/ClientHandler.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net.Security;
+using System.Threading;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Collections;
+using System.IO;
+
+namespace Server
+{
+	class ClientHandler
+	{
+		Server _server;
+		TcpClient _client;
+		NetworkStream _netStream;
+		SslStream _sslStream;
+		X509Certificate2 _cert;
+		BinaryReader _binaryReader;
+		BinaryWriter _binaryWriter;
+
+		public ClientHandler(Server server, TcpClient client, X509Certificate2 cert)
+		{
+			_client = client;
+			_server = server;
+			_cert = cert;
+			Thread t = new Thread(SetupConnection);
+			t.IsBackground = true;
+			t.Start();
+		}
+
+		void SetupConnection()
+		{
+			Console.WriteLine("New connection.");
+			_netStream = _client.GetStream();
+			_sslStream = new SslStream(_netStream, false);
+			_binaryReader = new BinaryReader(_sslStream, Encoding.UTF8);
+			_binaryWriter = new BinaryWriter(_sslStream, Encoding.UTF8);
+
+			try {
+				_sslStream.AuthenticateAsServer(_cert, false, SslProtocols.Tls, true); //kein client zertifikat, tls, ablaufdatum aber überprüfen 
+			} 
+			catch(AuthenticationException ex)
+			{
+				Console.WriteLine(ex.Message);
+				CloseConnection();
+			}
+			Console.WriteLine("Connection authenticated.");
+
+			_server.ClientList.Add(this);
+			Receive();
+
+		}
+
+		public void CloseConnection()
+		{
+			_binaryWriter.Close();
+			_binaryReader.Close();
+			_sslStream.Close();
+			_netStream.Close();
+			_client.Close();
+			Console.WriteLine("Connection closed.");
+		}
+
+		void Receive()
+		{
+			while (_client.Connected)
+			{
+				string msg = _binaryReader.ReadString();
+				_server.BroadcastMessage(this, msg);
+			}
+		}
+
+		public void Send(string msg)
+		{
+			if(_client.Connected)
+			{
+				_binaryWriter.Write(msg);
+			}
+		}
+	}
+}
---
+++ Server/Server.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Collections;
+
+namespace Server
+{
+	class Server
+	{
+		TcpListener _server;
+		IPAddress _ip = IPAddress.Parse("127.0.0.1");
+		int _port = 12345;
+		X509Certificate2 _cert = new X509Certificate2("Cert/selfSigned.pfx", "password");
+		List<ClientHandler> _clientList = new List<ClientHandler>();
+		bool _running = true;
+
+		public Server()
+		{
+			_server = new TcpListener(_ip, _port);
+			_server.Start();
+			Listen();
+		}
+
+		void Listen()
+		{
+			while (_running)
+			{
+				TcpClient tcpClient = _server.AcceptTcpClient();
+				ClientHandler clientHandler = new ClientHandler(this, tcpClient, _cert);
+			}
+		}
+
+		public void Close()
+		{
+			foreach(ClientHandler it in _clientList)
+			{
+				it.CloseConnection();
+			}
+		}
+
+		public void BroadcastMessage(ClientHandler sender, string msg)
+		{
+			foreach(ClientHandler it in _clientList) 
+			{
+				if (it != sender)
+				{
+					it.Send(msg);
+				}
+			}
+		}
+
+		public List<ClientHandler> ClientList
+		{
+			get { return _clientList; }
+		}
+	}
+}
